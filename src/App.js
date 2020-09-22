6dcfc71b26adbfeffde73778c44917f1add8b112
DavidSwanberg <davidswanberg767@gmail.com>
2020-09-22 14:53:22 -0400

    successfull api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 import Profile from './components/Profile';
 import Contact from './components/Contact';
 import Socials from './components/Socials';
 
 function App() {
+  const [user, setUser] = useState({});
+
+  const makeAPICall = async () => {
+    try {
+      const response = await axios('https://www.randomuser.me/api')
+      setUser(response.data.results[0])
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  
+  useEffect(() => {
+  makeAPICall()
+  }, [])
+
+
   return (
     <div className="App">
-      <Profile />
+      <Profile user={user}/>
       <Contact />
       <Socials />
     </div>
